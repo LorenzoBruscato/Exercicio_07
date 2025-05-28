f6c738c78335485493043916a2eea2b1de1c4a70
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-05-28 20:47:58 -0300

    Exercício_07

--- src/exercicio_07/Exercicio_07.java
+++ src/exercicio_07/Exercicio_07.java
@@ -8,10 +8,7 @@ import javax.swing.JOptionPane;
  */
 public class Exercicio_07 {
 
-    /**
-     * @param args the command line arguments
-     */
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog("Digite seu nome:");
         JOptionPane.showMessageDialog(null, "Seu nome foi armazenado com sucesso!");
         JOptionPane.showMessageDialog(null, "Seu nome é: " + nome);
